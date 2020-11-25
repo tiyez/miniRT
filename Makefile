2abaae734b5dc165a3f34883181d0c6e70161960
Joline Sandslash <jsandsla@va-h3.kzn.21-school.ru>
2020-11-25 18:48:33 +0300

    makefile_setup

--- Makefile
+++ Makefile
@@ -3,8 +3,13 @@ NAME=miniRT
 MLX_NAME=libmlx.a
 MLX_DIR=miniLibX/
 MLX=$(MLX_DIR)$(MLX_NAME)
+LIBFT_NAME=libft.a
+LIBFT_DIR=libft/
+LIBFT=$(LIBFT_DIR)$(LIBFT_NAME)
 
-FLAGS=-framework OpenGL -framework AppKit -I. -Wall -Wextra -Werror -Wno-deprecated-declarations
+FLAGS=\
+-framework OpenGL -framework AppKit \
+-I. -Wall -Wextra -Werror
 
 SRC=
 OBJ=$(SRC:.c=.o)
@@ -12,19 +17,29 @@ OBJ=$(SRC:.c=.o)
 all: $(NAME)
 
 %.o: %.c
-	gcc $(FLAGS) $(MLX) $< -o $@
+	gcc $(FLAGS) $(MLX) $(LIBFT) $< -o $@
 
-$(NAME): $(MLX) $(OBJ) main.c
-	gcc main.c $(FLAGS) -o $(NAME) $(OBJ) $(MLX)
+$(NAME): $(MLX)
+$(NAME): $(LIBFT)
+$(NAME): $(OBJ) main.c
+	gcc main.c $(FLAGS) -o $(NAME) $(OBJ) $(MLX) $(LIBFT)
 
 $(MLX):
 	$(MAKE) -C $(MLX_DIR)
 
+$(LIBFT):
+	$(MAKE) -C $(LIBFT_DIR)
+	$(MAKE) -C $(LIBFT_DIR) part1
+	$(MAKE) -C $(LIBFT_DIR) d
+
 clean:
 	rm -rf $(OBJ) main.o
 	$(MAKE) -C $(MLX_DIR) clean
+	$(MAKE) -C $(LIBFT_DIR) clean
 
 fclean: clean
 	rm -rf $(NAME)
+	$(MAKE) -C $(MLX_DIR) fclean
+	$(MAKE) -C $(LIBFT_DIR) fclean
 
 re: fclean all
--- main.c
+++ main.c
@@ -6,29 +6,124 @@
 /*   By: jsandsla <jsandsla@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 14:17:24 by jsandsla          #+#    #+#             */
-/*   Updated: 2020/11/25 16:13:35 by jsandsla         ###   ########.fr       */
+/*   Updated: 2020/11/25 18:30:35 by jsandsla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniLibX/mlx.h"
+#include "libft/libft.h"
+#include <stdio.h>
 
-#define INVALID ((void *)0)
+typedef struct	s_mouse
+{
+	int		x;
+	int		y;
+	int		dx;
+	int		dy;
+	int		left;
+	int		right;
+	int		middle;
+	int		scroll;
+}	t_mouse;
+
+typedef struct	s_super
+{
+	void		*mlx;
+	void		*winmlx;
+	t_mouse		mouse;
+	int			key;
+	int			exposed;
+}	t_super;
+
+void	prepare_super_events(t_super *super)
+{
+	int		x;
+	int		y;
+
+	mlx_mouse_get_pos(super->winmlx, &x, &y);
+	super->mouse.dx = x - super->mouse.x;
+	super->mouse.dy = y - super->mouse.y;
+	super->mouse.x = x;
+	super->mouse.y = y;
+}
+
+void	reset_super_events(t_super *super)
+{
+	super->exposed = 0;
+	super->key = -1;
+	super->mouse.left = 0;
+	super->mouse.right = 0;
+	super->mouse.middle = 0;
+	super->mouse.scroll = 0;
+}
+
+void	free_super(t_super **psuper)
+{
+	if (*psuper)
+	{
+		if ((*psuper)->mlx)
+			free((*psuper)->mlx);
+		if ((*psuper)->winmlx)
+			free((*psuper)->winmlx);
+		free(*psuper);
+	}
+	*psuper = 0;
+}
+
+int		loop_hook(t_super *super)
+{
+	prepare_super_events(super);
+	reset_super_events(super);
+	return (1);
+}
+
+int		expose_hook(t_super *super)
+{
+	super->exposed = 1;
+	printf("expose\n");
+	return (1);
+}
+
+int		key_hook(int keycode, t_super *super)
+{
+	super->key = keycode;
+	printf("key: %d (%c)\n", keycode, (char)keycode);
+	return (1);
+}
+
+int		mouse_hook(int button, int x, int y, t_super *super)
+{
+	if (button == 1)
+		super->mouse.left = 1;
+	else if (button == 2)
+		super->mouse.right = 1;
+	else if (button == 3)
+		super->mouse.middle = 1;
+	else if (button == 4 || button == 5)
+		super->mouse.scroll = (button - 4) * 2 - 1;
+	printf("mouse: btn: %d; x: %d; y: %d; scroll: %d;\n", button, x, y, super->mouse.scroll);
+	return (1);
+}
 
 int		main()
 {
-	void	*mlx;
-	void	*winmlx;
+	t_super	*super;
 
-	if ((mlx = mlx_init()) != INVALID)
+	if ((super = ft_calloc(1, sizeof(t_super))))
 	{
-		if ((winmlx = mlx_new_window(mlx, 800, 600, "Hello world")) != INVALID)
+		if ((super->mlx = mlx_init()))
 		{
-			mlx_clear_window(mlx, winmlx);
-			for (;;)
+			if ((super->winmlx = mlx_new_window(super->mlx,
+				800, 600, "Hello world!")))
 			{
-				mlx_loop(mlx);
+				mlx_mouse_hook(super->winmlx, mouse_hook, super);
+				mlx_key_hook(super->winmlx, key_hook, super);
+				mlx_expose_hook(super->winmlx, expose_hook, super);
+				mlx_loop_hook(super->mlx, loop_hook, super);
+				mlx_loop(super->mlx);
 			}
 		}
 	}
+	free_super(&super);
 }
 
